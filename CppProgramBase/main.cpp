88b0c48804f2dae672315681f9cb313210bd9953
granluz <granluz@kakao.com>
2022-10-24 00:18:12 +0900

    2차시 lab2

--- CppProgramBase/main.cpp
+++ CppProgramBase/main.cpp
@@ -4,11 +4,35 @@ using namespace std;
 
 int main() {
 
-
 	int num1, num2;
 	cout << "구구단의 범위를 입력 하십시오. :";
 	cin >> num1 >> num2;
-	if (num1 < num2)
+	
+	if (num1 > num2)
+	{
+		cout << "첫 번째 입력 숫자가 더 크므로, 시작과 끝의 값을 바꿔서 출력할까요(y/n)?";
+		char a;
+		cin >> a;
+		if (a == 'y')
+		{
+			int temp = num1;
+			num1 = num2;
+			num2 = temp;
+			for (int i = 1; i <= 9; i++)
+			{
+				for (int j = num1; j <= num2; j++)
+				{
+					cout << j << " * " << i << " = " << i * j << '\t';
+				}
+				cout << endl;
+			}
+		}
+		else if(a != 'y')
+		{
+			return 0;
+		}
+	}
+	else
 	{
 		for (int i = 1; i <= 9; i++)
 		{
@@ -18,10 +42,6 @@ int main() {
 			}
 			cout << endl;
 		}
-	}
-	else
-	{
-		cout << "두 번째 입력 숫자가 더 커야 합니다";
 	}
 	return 0;
 }
