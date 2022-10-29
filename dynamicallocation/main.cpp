b2fd0f66d36579a18307dd15c403b0f5c89770cc
granluz <granluz@kakao.com>
2022-10-29 14:45:12 +0900

    동적 할당

--- dynamicallocation/main.cpp
+++ dynamicallocation/main.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main()
 {
-	int* pl; // 동적 메모리를 가리키는 포인터
+	int* pi; // 동적 메모리를 가리키는 포인터
 
-	pl = new int; //1. 동적 메모리 할당
+	pi = new int; //1. 동적 메모리 할당
 
-	*pl = 100; // 2. 동적 메모리 사용
+	*pi = 100; // 2. 동적 메모리 사용
 
-	delete pl; // 3. 동적 메모리 반납
+	delete pi; // 3. 동적 메모리 반납
 
 	return 0;
 }
